b31be6e4add96bfa2ff19ffaf4a7994afabd8308
CodeObsess (Recursive In Develop-Code-Build-Sleep) <SpawnTree@users.noreply.github.com>
2017-07-30 01:52:19 +0530

    React Components

---
+++ server.js
@@ -0,0 +1,18 @@
+﻿var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+var Web3 = require('web3');
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+}).listen(3000, 'localhost', function(err) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+
+    
+});
